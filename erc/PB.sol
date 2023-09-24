4f6e788af045a47406f3210dcc68e69d297cd5e6
shishir kakhandki <shishir.kakhandki@gmail.com>
2023-09-24 20:03:13 +0530

    half

---
+++ erc/PB.sol
@@ -0,0 +1,88 @@
+// contracts/GLDToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "./IERC20.sol";
+
+contract PB is IERC20 {
+    using SafeMath for uint256;
+
+    string public constant name = "Pearly";
+    string public constant symbol = "PB";
+    uint8 public constant decimals = 0;
+
+    mapping(address => uint256) balances;
+    mapping(address => mapping(address => uint256)) allowed;
+
+    uint256 totalSupply_;
+
+    constructor(uint256 total) {
+        totalSupply_ = total;
+        balances[msg.sender] = totalSupply_;
+    }
+
+    function totalSupply() public view override returns (uint256) {
+        return totalSupply_;
+    }
+
+    function balanceOf(
+        address tokenOwner
+    ) public view override returns (uint256) {
+        return balances[tokenOwner];
+    }
+
+    function transfer(
+        address receiver,
+        uint256 numTokens
+    ) public override returns (bool) {
+        require(numTokens <= balances[msg.sender]);
+        balances[msg.sender] = balances[msg.sender].sub(numTokens);
+        balances[receiver] = balances[receiver].add(numTokens);
+        emit Transfer(msg.sender, receiver, numTokens);
+        return true;
+    }
+
+    function approve(
+        address delegate,
+        uint256 numTokens
+    ) public override returns (bool) {
+        allowed[msg.sender][delegate] = numTokens;
+        emit Approval(msg.sender, delegate, numTokens);
+        return true;
+    }
+
+    function allowance(
+        address owner,
+        address delegate
+    ) public view override returns (uint) {
+        return allowed[owner][delegate];
+    }
+
+    function transferFrom(
+        address owner,
+        address buyer,
+        uint256 numTokens
+    ) public override returns (bool) {
+        require(numTokens <= balances[owner]);
+        require(numTokens <= allowed[owner][msg.sender]);
+
+        balances[owner] = balances[owner].sub(numTokens);
+        allowed[owner][msg.sender] = allowed[owner][msg.sender].sub(numTokens);
+        balances[buyer] = balances[buyer].add(numTokens);
+        emit Transfer(owner, buyer, numTokens);
+        return true;
+    }
+}
+
+library SafeMath {
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
+
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        assert(c >= a);
+        return c;
+    }
+}
